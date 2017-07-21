c1e1030955d2ae05b9132d787bed178c0b874a93
SriSasanka <srisasanka.is@gmail.com>
2017-07-21 09:25:29 +0530

    committed by sasanka on 20/7/17

--- src/app/recipies/recipies-list/recipies-list.component.ts
+++ src/app/recipies/recipies-list/recipies-list.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipies-list',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipiesListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe' , ' This is Simply a Test', 'https://s-media-cache-ak0.pinimg.com/originals/20/dc/11/20dc1144bb48449a92445a21a4cdaff4.jpg')
+  ];
 
   constructor() { }
 
